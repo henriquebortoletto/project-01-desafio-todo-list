7a87d6faf8fff40faa51c53d8fb0f7391ecd6215
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-04-11 19:42:11 -0300

    feat(<app />): 🚀 defining styles for the main section and formatting the tasks form 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
+import { PlusCircle } from '@phosphor-icons/react'
+
 import Header from '@/components/Header'
 
+import S from '@/app.module.css'
 import './global.css'
 
 function App() {
+
   return (
     <>
       <Header />
-      <main>Main</main>
+      <main className={S.container}>
+        <form className={S.form}>
+          <div className={S.group_input}>
+            <label
+              htmlFor="task"
+              className="sr-only"
+            >
+              Adicione uma nova tarefa
+            </label>
+            <input
+              id="task"
+              type="text"
+              placeholder="Adicione uma nova tarefa"
+            />
+          </div>
+          <button type="submit">
+            <span>Criar</span>
+            <PlusCircle size={16} />
+          </button>
+        </form>
+
+        <section>
+          <div>...header</div>
+          <div>...tasks</div>
+        </section>
+      </main>
     </>
   )
 }
