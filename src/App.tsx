9fce9079c94f686e19d67e2d995b393dcb2dc443
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-04-11 18:46:27 -0300

    feat(<header />): 🚀 add component header 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Header from '@/components/Header'
+
 import './global.css'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <main>Main</main>
+    </>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import logoRocket from '@/assets/rocket.svg'
+
+import S from './styles.module.css'
+
+const Header = () => {
+  return (
+    <header className={S.header}>
+      <div className={S.container}>
+        <img
+          src={logoRocket}
+          alt="Icone de um foguete decolando"
+          loading="lazy"
+        />
+        <h1>
+          <span>to</span>
+          <span>do</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
