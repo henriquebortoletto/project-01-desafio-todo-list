032a90e2684f3b994827ae4d9519d2a7287cd5d9
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-04-11 21:12:08 -0300

    refactor(<app />): 🚀 rename app.module for App.module 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header'
 import Task from '@/components/Task'
 import Empty from '@/components/Empty'
 
-import S from '@/app.module.css'
+import S from '@/App.module.css'
 import './global.css'
 
 
