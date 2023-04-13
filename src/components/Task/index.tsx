4d9ad037e391f19ec8caaa8d45a5366bb359ef00
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-04-13 19:02:25 -0300

    feat(styles): 🚀 defining hover states of elements and added useId hook to generate unique ids 🚀

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,13 +1,20 @@
+import { useId } from 'react'
 import { Trash } from '@phosphor-icons/react'
 
 import S from './styles.module.css'
 
 function Task() {
+  const uniqueIdPerTask = useId();
+
   return (
     <div className={S.task}>
       <form>
-        <label htmlFor="task">
-          <input id="task" type="radio" />
+        <label htmlFor={uniqueIdPerTask}>
+          <input
+            id={uniqueIdPerTask}
+            className="sr-only"
+            type="checkbox"
+          />
           <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
         </label>
       </form>
