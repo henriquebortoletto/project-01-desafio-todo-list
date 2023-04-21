3c00abd541619f9ffb611c0d39657e02c6029457
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-04-21 17:46:29 -0300

    refactor(mock): 🚀 updating the mock example 🚀
    ✓ Updating the mock example used in creating the task to get id
      using uuid instead of an incremental;

--- src/data.ts
+++ src/data.ts
@@ -2,27 +2,27 @@ import { TaskProps } from '@/components/Task'
 
 export const tasks: TaskProps[] = [
   {
-    id: "1",
+    id: "a4a578e1-35ad-468a-8028-d9b7aa62b75f",
     title: "#1 - Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
     completed: false
   },
   {
-    id: "2",
+    id: "1c8082f7-579e-4639-94e9-57c3dd418038",
     title: "#2 - Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
     completed: false
   },
   {
-    id: "3",
+    id: "966fecfe-5e89-4696-a6e0-e6b093486299",
     title: "#3 - Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
     completed: false
   },
   {
-    id: "4",
+    id: "aba9a43b-3da2-4b58-9883-3b6a6752d4b2",
     title: "#4 - Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
     completed: true
   },
   {
-    id: "5",
+    id: "4807c70a-3f4e-48d1-bd48-bdac40ff3587",
     title: "#5 - Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
     completed: true
   }
